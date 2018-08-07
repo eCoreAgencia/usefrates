17f1ffcc9171d5e6c696e04f8f34b9e9704e2ea0
Felipe Pereira <lf.pereirarj@gmail.com>
2018-08-07 19:55:45 -0300

    init

---
+++ config/variables.js
@@ -0,0 +1,9 @@
+const pjson = require('../package.json');
+
+module.exports = {
+  PROJECT_ID: "vtex-structure",
+  TEMPLATE_PREFIX: "eCore-",
+  PROJECT_VERSION: pjson.version,
+  STORE_ID: "vtex-structure",
+  STORE_NAME: "Vtex Structure"
+}
