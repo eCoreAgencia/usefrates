c01a4fe1974b1d1db07eabd7bbf785d62852a5e5
Ronald Junger <ronaldjunger@gmail.com>
2018-11-07 17:21:47 -0200

    catalog

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../scss/main.scss';
 import './pages/home';
 import './pages/catalog';
+import './pages/empty-search';
 
 import './components/filter';
 import './components/minicart';
--- src/js/pages/empty-search.js
+++ src/js/pages/empty-search.js
@@ -1,24 +1,102 @@
-$(document).ready(function() {
-	if ($('body').hasClass('empty-search')) {
-		var word = decodeURI(window.location.search);
-		word = word.replace('?ft=', '');
-		console.log(word);
-		$('.box-emptySearch p span em').text(word);
+$(document).ready(() => {
+    if ($('body').hasClass('empty-search')) {
+        let word = decodeURI(window.location.search);
+        word = word.replace('?ft=', '');
+        console.log(word);
+        $('.section__title h2 em').text(word);
+    }
+});
 
-		$('.shelf__carousel--full ul').slick({
-			arrows: true,
-			slideToShow: 4,
-			slidesToScroll: 1,
-			infinite: true,
-			variableWidth: true,
-			prevArrow: shelf__prev,
-			nextArrow: shelf__next,
-			responsive: [
-				{
-					breakpoint: 800,
-					settings: 'unslick'
-				}
-			]
-		});
-	}
+$(document).ready(() => {
+    if ($('body').hasClass('empty-search')) {
+        if ($('li.helperComplement')[0]) {
+            $('li.helperComplement').remove();
+        }
+
+        const shelf__prev = `<button type='button' class='slick-prev shelf__button'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="43" height="43" viewBox="0 0 43 43"><defs><path id="vcuya" d="M1460 1326.21l21.21-21.21 21.21 21.21-21.21 21.21z"/><path id="vcuyc" d="M1481.5 1318.5l-7.52 7.52"/><path id="vcuyd" d="M1481.5 1333.02l-7.52-7.52"/><clipPath id="vcuyb"><use fill="#fff" xlink:href="#vcuya"/></clipPath></defs><g><g transform="matrix(-1 0 0 1 1503 -1305)"><g><use fill="#fff" fill-opacity="0" stroke="#000" stroke-miterlimit="50" stroke-width="4" clip-path="url(&quot;#vcuyb&quot;)" xlink:href="#vcuya"/></g><g><g><use fill="#fff" fill-opacity="0" stroke="#000" stroke-linecap="square" stroke-miterlimit="50" stroke-width="2" xlink:href="#vcuyc"/></g><g><use fill="#fff" fill-opacity="0" stroke="#000" stroke-linecap="square" stroke-miterlimit="50" stroke-width="2" xlink:href="#vcuyd"/></g></g></g></g></svg></button>`
+        const shelf__next = `<button type='button' class='slick-next shelf__button'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="43" height="43" viewBox="0 0 43 43"><defs><path id="vcuya" d="M1460 1326.21l21.21-21.21 21.21 21.21-21.21 21.21z"/><path id="vcuyc" d="M1481.5 1318.5l-7.52 7.52"/><path id="vcuyd" d="M1481.5 1333.02l-7.52-7.52"/><clipPath id="vcuyb"><use fill="#fff" xlink:href="#vcuya"/></clipPath></defs><g><g transform="matrix(-1 0 0 1 1503 -1305)"><g><use fill="#fff" fill-opacity="0" stroke="#000" stroke-miterlimit="50" stroke-width="4" clip-path="url(&quot;#vcuyb&quot;)" xlink:href="#vcuya"/></g><g><g><use fill="#fff" fill-opacity="0" stroke="#000" stroke-linecap="square" stroke-miterlimit="50" stroke-width="2" xlink:href="#vcuyc"/></g><g><use fill="#fff" fill-opacity="0" stroke="#000" stroke-linecap="square" stroke-miterlimit="50" stroke-width="2" xlink:href="#vcuyd"/></g></g></g></g></svg></button>`
+    
+        $('.shelf__carousel--full ul').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            prevArrow: shelf__prev,
+            nextArrow: shelf__next,
+            responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+        });
+
+        // $('.shelf__carousel--full ul').slick({
+        //     arrows: true,
+        //     slideToShow: 1,
+        //     slidesToScroll: 1,
+        //     dots: true,
+        //     infinite: true,
+        //     prevArrow: shelf__prev,
+        //     nextArrow: shelf__next,
+        //     responsive: [{
+        //             breakpoint: 1024,
+        //             settings: {
+        //                 slidesToShow: 4,
+        //                 slidesToScroll: 2,
+        //                 dots: false,
+        //                 infinite: true,
+        //             },
+        //         },
+        //         {
+        //             breakpoint: 800,
+        //             settings: {
+        //                 slidesToShow: 3,
+        //                 slidesToScroll: 3,
+        //                 dots: false,
+        //                 infinite: true,
+        //                 arrows: false,
+        //             },
+        //         },
+        //         {
+        //             breakpoint: 600,
+        //             settings: {
+        //                 slidesToShow: 2,
+        //                 slidesToScroll: 2,
+        //                 infinite: true,
+        //                 arrows: false,
+        //             },
+        //         },
+        //         {
+        //             breakpoint: 480,
+        //             settings: {
+        //                 slidesToShow: 1,
+        //                 slidesToScroll: 1,
+        //                 dots: false,
+        //                 infinite: true,
+        //                 arrows: false,
+        //             },
+        //         },
+        //         // You can unslick at a given breakpoint now by adding:
+        //         // settings: "unslick"
+        //         // instead of a settings object
+        //     ],
+        // });
+    }
 });
