d77d55a9d78386b1a5570d54c47bb3f28c8591e5
Felipe Pereira <lf.pereirarj@gmail.com>
2018-11-09 20:18:54 -0200

    sku

--- src/js/components/product.js
+++ src/js/components/product.js
@@ -1,7 +1,7 @@
 import {
 	getProductWithVariations
 } from '../modules/vtexRequest';
-import { isMobile } from '../utils';
+import { isMobile,slugify } from '../utils';
 class Product {
 	constructor() {
 		const productId = $('#___rc-p-id').val();
@@ -15,8 +15,36 @@ class Product {
 	}
 
 	renderSkuSelectors(product) {
-		console.log(product);
-	}
+        console.log(product);
+        const select = `
+            <div class="product__skus--size">
+                <span class="product__skus-title">Tamanho</span>
+                <select name="sku-size">
+                    ${this.createSkuSelect(product.dimensionsMap.Tamanho)}
+                </select>
+            </div>`;
+        const list = `
+            <div class="product__skus--color">
+                <span class="product__skus-title">Cor</span>
+                <ul>
+                    ${this.createSkuThumb(product.dimensionsMap.Cor)}
+                </ul>
+            </div>`;
+        const skus = `<div class="product__skus-inner">
+                ${list}
+                ${select}
+        </div>`
+        $('.product__skus').html(skus);
+        $(window).trigger('skuSelectorCreated');
+    }
+    
+    createSkuSelect(dimensions){
+        return dimensions.map( dimension => `<option value="${dimension}">${dimension}</option>`).join('');
+    }
+
+    createSkuThumb(dimensions){
+        return dimensions.map( dimension => `<li><label for="${slugify(dimension)}">${dimension}</label><input type="radio" id="${slugify(dimension)}" name="sku-color" value="${dimension}"></li>`).join('');
+    }
 }
 
 $(document).ready(() => {
@@ -27,7 +55,56 @@ $(document).ready(() => {
                 arrows: false,
                 dots: true
             });
-        }    
+        } 
+        
+        
+
+        $(window).on('skuSelectorCreated', () => {
+            $('select').each(function(){
+                var $this = $(this), numberOfOptions = $(this).children('option').length;
+        
+                $this.addClass('select-hidden');
+                $this.wrap('<div class="select"></div>');
+                $this.after('<div class="select-styled"></div>');
+        
+                var $styledSelect = $this.next('div.select-styled');
+                $styledSelect.text($this.children('option').eq(0).text());
+        
+                var $list = $('<ul />', {
+                    'class': 'select-options'
+                }).insertAfter($styledSelect);
+        
+                for (var i = 0; i < numberOfOptions; i++) {
+                    $('<li />', {
+                        text: $this.children('option').eq(i).text(),
+                        rel: $this.children('option').eq(i).val()
+                    }).appendTo($list);
+                }
+        
+                var $listItems = $list.children('li');
+        
+                $styledSelect.click(function(e) {
+                    e.stopPropagation();
+                    $('div.select-styled.active').not(this).each(function(){
+                        $(this).removeClass('active').next('ul.select-options').hide();
+                    });
+                    $(this).toggleClass('active').next('ul.select-options').toggle();
+                });
+        
+                $listItems.click(function(e) {
+                    e.stopPropagation();
+                    $styledSelect.text($(this).text()).removeClass('active');
+                    $this.val($(this).attr('rel'));
+                    $list.hide();
+                });
+        
+                $(document).click(function() {
+                    $styledSelect.removeClass('active');
+                    $list.hide();
+                });
+        
+            });
+        });
     }
 	
 })
