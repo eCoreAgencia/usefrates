197a6eb1b61fc4ad3b69b21133e238a345c93572
Felipe Pereira <lf.pereirarj@gmail.com>
2018-11-19 10:30:50 -0200

    ajustes

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,43 +1,43 @@
 class MenuMobile {
-    constructor(){
+	constructor() {
 
-    }
-    openMenu(element){
-        element.addClass('is-open');
-    }
-    closeMenu(element){
-        element.removeClass('is-open');
-        $('.menu--level').removeClass('is-open');
-    }
+	}
+	openMenu(element) {
+		element.addClass('is-open');
+	}
+	closeMenu(element) {
+		element.removeClass('is-open');
+		$('.menu--level').removeClass('is-open');
+	}
 
-    openSubmenu(element) {
-        const submenu = element.data('submenu');
+	openSubmenu(element) {
+		const submenu = element.data('submenu');
 
-        $(`.${submenu}`).addClass('is-open');
-    }
-    closeSubmenu(element) {
-       $(element).removeClass('is-open');
-    }
+		$(`.${submenu}`).addClass('is-open');
+	}
+	closeSubmenu(element) {
+		$(element).removeClass('is-open');
+	}
 }
 
 
-$(document).ready(function(){
-    const menuMobile = new MenuMobile();
-    const headerMenu = $('.header__menu');
-    $('.btn--menu').on('click', () => {
-        menuMobile.openMenu(headerMenu)
-    });
-   $('.btn--close').on('click', () => {
-        menuMobile.closeMenu(headerMenu)
-   });
+$(document).ready(function () {
+	const menuMobile = new MenuMobile();
+	const headerMenu = $('.header__menu');
+	$('.btn--menu').on('click', () => {
+		menuMobile.openMenu(headerMenu)
+	});
+	$('.btn--close').on('click', () => {
+		menuMobile.closeMenu(headerMenu)
+	});
 
-   $('.menu__item--dropdown').on('click', function(e){
-        e.preventDefault();
-        menuMobile.openSubmenu($('.menu__link', this));
-   })
+	$('.menu__item--dropdown > .menu__link').on('click', function (e) {
+		e.preventDefault();
+		menuMobile.openSubmenu($(this));
+	})
 
-   $('.menu__item--dropup').on('click', function(e){
-        e.preventDefault();
-        menuMobile.closeSubmenu($(this).parent());
-    })
+	$('.menu__item--dropup').on('click', function (e) {
+		e.preventDefault();
+		menuMobile.closeSubmenu($(this).parent());
+	})
 })
